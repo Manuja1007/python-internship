32ea28ecbfc30fac8a7be91b76bc3fd2ec452549
Manuja1007 <manujasawant694@gmail.com>
2025-02-19 20:32:11 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,14 @@
+print("Hello welcome to the fest AARAMBH! thank you for coming everyone, I hope you have a great day ahead")
+print("Here is the list of sports will be held during fest:")
+print("list=['Kabbadi','Kho-kho','Badminton','Basketball','Cricket','Volleyball']")
+print("AND THE MOST AWAITED DJ NIGHT!!! We will be having a grand consert of KRATEX!!!!")
+name=input("Enter your name:")
+print("1.SPORTS")
+print("2.DJ NIGHT")
+choice=input("Enter your choice(1 or 2)")
+if choice=="1":
+    print("Please proceed towards ground and let respective incharge know your sports specification!")
+elif choice=="2":   
+    print("Please proceed towards OS lab for collecting your pass!") 
+else:
+    print("INVALID CHOICE")
